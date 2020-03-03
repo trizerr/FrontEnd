f36af48c5e4be77447f434308823be63af979a63
trizerr <55409062+trizerr@users.noreply.github.com>
2020-03-03 01:12:47 -0800

    Calculator fixed
    Tests in progress

--- stud/src/calculator/script.js
+++ stud/src/calculator/script.js
@@ -1,74 +1,69 @@
-let numbers=[], operations=[], numbersIndex=0, operationsIndex=0;
-let i=0, j=0, input, temp="", result=false;
+let number1 = 0, number2, input, operationCheck = false, operation, result, dotCheck = false;
 input = document.getElementById("input");
-preResult = document.getElementById("preResult");
-let value = input.value;
-let name;
 
-const buttons = document.querySelectorAll(".number")
+const buttons = document.querySelectorAll(".number");
 for (const button of buttons) {
     button.addEventListener('click', function(event) {
-        input.value += button.innerHTML;
-        value = input.value;
-        Result();
+        if(operationCheck == true) {
+            if(button.innerHTML == "."){
+                if(dotCheck == false){
+                    operation = input.value;
+                    input.value = "";
+                    input.value += button.innerHTML;
+                    operationCheck = false;
+                    dotCheck = true;
+                }
+            }
+            operation = input.value;
+
+            input.value = "";
+            input.value += button.innerHTML;
+            operationCheck = false;
+        }
+        else {
+            if(button.innerHTML != ".")
+            input.value += button.innerHTML;
+            else
+            {
+                if(dotCheck == false) {
+                    input.value += button.innerHTML;
+                    dotCheck = true;
+                }
+            }
+        }
     })
 }
+
 const operationss = document.querySelectorAll(".operation")
 for (const operation of operationss) {
     operation.addEventListener('click', function(event) {
-        input.value += operation.innerHTML;
-        operations[i] = operation.innerHTML;
-        i++;
-        value = input.value;
-        Result();
-    })
-}
-document.getElementById("backspace").onclick = function() {
-    value = input.value;
-    input.value=value.substring(0,value.length-1);
-    value = input.value;
-    Result();
-}
+        if(number1==0) {
+            operationCheck = true;
+            number1 = parseFloat(input.value);
+            input.value = operation.innerHTML;
+            dotCheck = false;
+        }
+        })
+    }
+
 
 document.getElementById("AC").onclick = function(){
-    input.value = "";
-    i=0;
-    numbers=[];
-    operations=[];
-    value = input.value;
-    Result();
-}
-document.getElementById("=").onclick = function(){
-    value = input.value;
-    result=true;
-    Result();
-}
-function Result(){
-    getValue(value);
-    preResult.innerText = calc(numbers, operations);
-    if(result) {
-        input.value = preResult.innerText;
-        preResult.innerText = "0";
-    }
-    result = false;
-    i=0;
-    numbersIndex=0;
-    operationsIndex=0;
+  number1 = 0;
+  number2 = 0;
+  operation = 0;
+  input.value = "";
+  dotCheck = false;
 }
 
-function getValue(value){
-    numbers[numbersIndex]="";
-    for(let i = 0; i < value.length; i++){
-        if(value[i]=="+" || value[i]=="-" || value[i]=="/" || value[i]=="*" || value[i]=="%"){
-            operations[operationsIndex] = value[i];
-            operationsIndex++;
-            numbersIndex++;
-            numbers[numbersIndex] = "";
-        } else{
-            numbers[numbersIndex] += value[i];
-        }
-    }
+document.getElementById("=").onclick = function(){
+    number2 = parseInt(input.value);
+    console.log(number1,number2,operation);
+    input.value = calc();
+    number1 = 0;
+    number2 = 0;
+    operation = 0;
 }
+
 function sum(a, b){
     return a+b;
 }
@@ -81,47 +76,19 @@ function multiplication(a, b){
 function substruction(a, b){
     return a-b;
 }
-function remainder(a, b){
-    return a%b;
-}
-function calc(numbers, operations){
-    numbers=numbers.map(Number);
-    for(let i = 0; i <= numbers.length; i++) {
-            if (operations[i] == "*") {
-                numbers[i] = multiplication(numbers[i], numbers[i + 1]);
-                numbers.splice(i+1, 1);
-                operations.splice(i, 1);
-                i--;
-            }
-            if (operations[i] == "/") {
-                numbers[i] = division(numbers[i], numbers[i + 1]);
-                numbers.splice(i+1, 1);
-                operations.splice(i, 1);
-                i--;
-            }
-    }
-    for(let i = 0; i < numbers.length; i++) {
-            if (operations[i] == "+") {
 
-                numbers[i] = sum(numbers[i], numbers[i + 1]);
-                numbers.splice(i+1, 1);
-                operations.splice(i, 1);
-                i--;
-
-            }
-            if (operations[i] == "-") {
-                numbers[i] = substruction(numbers[i], numbers[i + 1]);
-                numbers.splice(i+1, 1);
-                operations.splice(i, 1);
-                i--;
-            }
-            if (operations[i] == "%") {
-              numbers[i] = remainder(numbers[i], numbers[i + 1]);
-              numbers.splice(i+1, 1);
-              operations.splice(i, 1);
-              i--;
-            }
-    }
-    let result=numbers[0];
-    return result;
+function calc(){
+     if(operation == "+"){
+         result = sum(number1, number2);
+     }
+     if(operation == "/"){
+         result = division(number1, number2);
+     }
+     if(operation == "-"){
+         result = substruction(number1, number2);
+     }
+     if(operation == "*"){
+         result = multiplication(number1, number2);
+     }
+     return result;
 }
